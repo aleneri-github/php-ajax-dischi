1d06ee8bd3f4606d496df24d471b354ff52f1eb6
Alessandro <aleneri242@gmail.com>
2021-01-22 17:43:42 +0100

    Aggiornamento struttura

--- index.php
+++ index.php
@@ -71,13 +71,14 @@ $dischi = [
     "year"=> "1987"
   ],
 ];
-
-var_dump($dischi);
- ?>
+?>
 
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
+    <!-- Google Font -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+SC&family=Roboto&family=Yusei+Magic&display=swap" rel="stylesheet">
     <!-- CDN Font Awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w==" crossorigin="anonymous" />
     <!-- Mio foglio di stile -->
@@ -91,16 +92,14 @@ var_dump($dischi);
     </header>
 
     <main>
-      <div class="disc-box"></div>
-      <div class="disc-box"></div>
-      <div class="disc-box"></div>
-      <div class="disc-box"></div>
-      <div class="disc-box"></div>
-      <div class="disc-box"></div>
-      <div class="disc-box"></div>
-      <div class="disc-box"></div>
-      <div class="disc-box"></div>
-      <div class="disc-box"></div>
+      <div class="disc-box">
+        <div class="cover">
+        </div>
+        <h2>Titolo Album</h2>
+        <h3>Artista</h3>
+        <h4>Anno</h4>
+      </div>
+
 
 
     </main>
