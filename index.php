c76d6e5efd2b4658128aeb215e5416c9ecbabc90
Alessandro <aleneri242@gmail.com>
2021-01-22 16:31:14 +0100

    nuovo aggiornamento

--- index.php
+++ index.php
@@ -1,13 +1,17 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
+    <!-- CDN Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w==" crossorigin="anonymous" />
+    <!-- Mio foglio di stile -->
     <link rel="stylesheet" href="css/app.css">
     <meta charset="utf-8">
     <title>Dischi</title>
   </head>
   <body>
     <header>
-      
+      <i class="fab fa-spotify"></i>
+
     </header>
 
   </body>
